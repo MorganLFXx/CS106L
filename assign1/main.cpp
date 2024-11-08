35adc94339987cb4144d00042d0efea0ebda0f00
221850120-Zhou HongKai <221850120@smail.nju.edu.cn>
2024-11-08 20:49:05 +0800

    a1 补充了Course结构体

--- assign1/main.cpp
+++ assign1/main.cpp
@@ -24,18 +24,12 @@ const std::string COURSES_NOT_OFFERED_PATH = "student_output/courses_not_offered
  * You must fill in the types of the fields in this struct.
  * Hint: Remember what types C++ streams work with?!
  */
-struct Course {
-  /* STUDENT TODO */ title;
-  /* STUDENT TODO */ number_of_units;
-  /* STUDENT TODO */ quarter;
+struct Course
+{
+  std::string title;
+  int number_of_units;
+  std::string quarter;
 };
-
-/**
- * (STUDENT TODO) Look at how the main function (at the bottom of this file)
- * calls `parse_csv`, `write_courses_offered`, and `write_courses_not_offered`.
- * Modify the signatures of these functions so that they work as intended, and then delete this
- * comment!
- */
 
 /**
  * Note:
@@ -58,7 +52,8 @@ struct Course {
  * @param filename The name of the file to parse.
  * @param courses  A vector of courses to populate.
  */
-void parse_csv(std::string filename, std::vector<Course> courses) {
+void parse_csv(std::string filename, std::vector<Course> courses)
+{
   /* (STUDENT TODO) Your code goes here... */
 }
 
@@ -80,7 +75,8 @@ void parse_csv(std::string filename, std::vector<Course> courses) {
  * @param all_courses A vector of all courses gotten by calling `parse_csv`.
  *                    This vector will be modified by removing all offered courses.
  */
-void write_courses_offered(std::vector<Course> all_courses) {
+void write_courses_offered(std::vector<Course> all_courses)
+{
   /* (STUDENT TODO) Your code goes here... */
 }
 
@@ -97,11 +93,13 @@ void write_courses_offered(std::vector<Course> all_courses) {
  *
  * @param unlisted_courses A vector of courses that are not offered.
  */
-void write_courses_not_offered(std::vector<Course> unlisted_courses) {
+void write_courses_not_offered(std::vector<Course> unlisted_courses)
+{
   /* (STUDENT TODO) Your code goes here... */
 }
 
-int main() {
+int main()
+{
   /* Makes sure you defined your Course struct correctly! */
   static_assert(is_valid_course<Course>, "Course struct is not correctly defined!");
 
